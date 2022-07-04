d6ad572698ac48d571eb1515708e8627d10b4262
marco_rivia <marco1088@hotmail.nl>
2022-07-04 15:02:59 +0200

    Add main class

---
+++ src/main/java/main.java
@@ -0,0 +1,24 @@
+public class main {
+
+    public static void main(String args[]){
+
+        // State pattern
+
+
+        // Observer
+
+
+        // Composite / visitor -> Niek
+
+
+        // Strategy
+
+
+        // Adapter
+
+
+        // Factory
+
+    }
+
+}
